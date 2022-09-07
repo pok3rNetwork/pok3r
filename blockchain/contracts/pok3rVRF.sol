57095124d0ea70a6f9bd2d55a4b2b36387183de7
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-09-07 14:55:32 -0700

    v0.1

--- blockchain/contracts/pok3rVRF.sol
+++ blockchain/contracts/pok3rVRF.sol
@@ -5,7 +5,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract pok3rVRF is VRFConsumerBaseV2 {
+contract POK3RVRF is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface COORDINATOR;
     LinkTokenInterface LINKTOKEN;
 
