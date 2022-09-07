d36c63f9d69c1d766aa50f7ccf60aac080618306
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-09-07 14:33:22 -0700

    init base contracts

---
+++ blockchain/contracts/pok3r.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
---
+++ client/README.md
@@ -0,0 +1,3 @@
+# TechStack?
+
+MERN
