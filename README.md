c24f766a189d76ffc549ab9a7bf4d38d1a4f7a5d
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-09-09 16:39:33 -0700

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# War
+
+https://en.wikipedia.org/wiki/War_(card_game)
+
+There's no secret information so we don't have to worry about hashes etc.
+Simple War game to show you guys and get feedback or center team raoudn where we want to go
--- blockchain/contracts/pok3rVRF.sol
+++ blockchain/contracts/pok3rVRF.sol
@@ -1,55 +1,55 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.16;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
 
-// import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-// import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-// import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-// contract POK3RVRF is VRFConsumerBaseV2 {
-//     VRFCoordinatorV2Interface COORDINATOR;
-//     LinkTokenInterface LINKTOKEN;
+contract POK3RVRF is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+    LinkTokenInterface LINKTOKEN;
 
-//     address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
-//     address link_token_contract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-//     bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
-//     uint32 callbackGasLimit = 2500000;
-//     uint16 requestConfirmations = 3;
-//     uint32 numWords =  1;
-//     uint256[] public s_randomWords;
-//     uint256 public s_requestId;
-//     uint64 public s_subscriptionId;
-//     address s_owner;
-//     mapping(uint256 => bool) public isRequestFulfilled;
+    address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+    address link_token_contract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    bytes32 keyHash =
+        0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    uint32 callbackGasLimit = 2500000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    uint64 public s_subscriptionId;
+    address s_owner;
+    mapping(uint256 => bool) public isRequestFulfilled;
 
-//     constructor(uint256 _subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
-//     COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-//     LINKTOKEN = LinkTokenInterface(link_token_contract);
-//     s_owner = msg.sender;
-//     s_subscriptionId = uint64(_subscriptionId);
-//   }
+    constructor(uint256 _subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        LINKTOKEN = LinkTokenInterface(link_token_contract);
+        s_owner = msg.sender;
+        s_subscriptionId = uint64(_subscriptionId);
+    }
 
-//   // Assumes the subscription is funded sufficiently.
-//   function requestRandomWords() external onlySubOwner {
-//     // Will revert if subscription is not set and funded.
-//     s_requestId = COORDINATOR.requestRandomWords(
-//       keyHash,
-//       s_subscriptionId,
-//       requestConfirmations,
-//       callbackGasLimit,
-//       numWords
-//     );
-//   }
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords() external onlySubOwner {
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
 
-//   function fulfillRandomWords(
-//     uint256, /* requestId */
-//     uint256[] memory randomWords
-//   ) internal override {
-//     s_randomWords = randomWords;
-//   }
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
+    }
 
-
-//   modifier onlySubOwner() {
-//     require(msg.sender == s_owner);
-//     _;
-//   }
-// }
+    modifier onlySubOwner() {
+        require(msg.sender == s_owner);
+        _;
+    }
+}
