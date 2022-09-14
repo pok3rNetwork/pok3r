cdedb58e189240d09d83881f59194b510e16ad0d
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-09-14 13:59:47 -0500

    demo backend poker game

--- api/app.js
+++ api/app.js
@@ -1,29 +1,28 @@
-require("dotenv").config();
-const port = 8001; // env?
-const url = process.env.CLIENT_URL;
+require('dotenv').config();
+const port = process.env.PORT ? process.env.PORT : 8081; // env?
+const url = process.env.CLIENT_URL
+  ? process.env.CLIENT_URL
+  : `http://localhost:${port}/poker/`;
 
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { routeGame } = require('./poker/game.js');
 
-function configureApp() {
-  const application = express();
-  application.use(bodyParser.json());
-  application.use(cors({ origin: url }));
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: url }));
 
-  if (!fs.existsSync("./static")) fs.mkdirSync("./static");
-  application.use("/static", express.static("static"));
+if (!fs.existsSync('./static')) fs.mkdirSync('./static');
+app.use('/static', express.static('static'));
 
-  application.listen(port, () => {
-    console.log("Server Started on Port:" + port);
-  });
-  return application;
-}
-
-const app = configureApp();
-
-const welcomeMessage = "Hello World!";
-app.get("/", (req, res) => {
-  res.json({ welcomeMessage, url });
+app.listen(port, () => {
+  console.log('Server Started on Port:' + port);
 });
+
+app.get('/', (req, res) => {
+  res.json({ welcomeMessage: 'Hello World!', url });
+});
+
+routeGame(app, __dirname);
