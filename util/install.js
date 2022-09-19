7a4fc7c508ddf3eccedd72649e1e5f49fe559b2e
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-09-19 12:22:07 -0500

    install utility

--- util/install.js
+++ util/install.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const { powershell } = require('./shell.js');
 
 (async () => {
+  if (!fs.existsSync(`./node_modules`)) {
+    console.log(`Running Install on project root`);
+    const opts = { args: ['./'], v: true };
+    await powershell('npm/install', opts);
+  }
+
   var subRepos = ['api', 'blockchain', 'client'];
   for await (const repo of subRepos) {
     const pathTo = `./${repo}`;
