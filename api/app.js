9761bf6cefddd2b3a3e93ecf21c61e53f8310cfe
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-09-21 10:34:56 -0500

    index adjustment

--- api/app.js
+++ api/app.js
@@ -1,29 +1,10 @@
-require('dotenv').config();
-const port = process.env.API_PORT ? process.env.API_PORT : 8081;
-const url = process.env.CLIENT_URL
-  ? process.env.CLIENT_URL
-  : `http://localhost:${port}/poker/`;
-
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const { port, initApp } = require('./utils/setup.js');
 const poker = require('./poker/route.js');
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors({ origin: url }));
-
-if (!fs.existsSync(`${__dirname}/static/lobbies`))
-  fs.mkdirSync(`${__dirname}/static/lobbies`, { recursive: true });
-app.use('/static', express.static('static'));
+const app = initApp(__dirname);
 
 app.listen(port, () => {
   console.log('Server Started on Port:' + port);
-});
-
-app.get('/', (req, res) => {
-  res.json({ welcomeMessage: 'Hello World!', url });
 });
 
 poker(app, __dirname);
